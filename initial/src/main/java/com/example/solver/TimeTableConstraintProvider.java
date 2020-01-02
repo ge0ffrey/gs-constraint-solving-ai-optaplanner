66dbdf091463be99280c123b6b6b68d61db8a8c3
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-01-02 17:35:18 +0100

    Change package to "com.example"

---
+++ initial/src/main/java/com/example/solver/TimeTableConstraintProvider.java
@@ -0,0 +1,54 @@
+package com.example.solver;
+
+import com.example.domain.Lesson;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+import org.optaplanner.core.api.score.stream.Joiners;
+
+public class TimeTableConstraintProvider implements ConstraintProvider {
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[] {
+                // Hard constraints
+                roomConflict(constraintFactory),
+                teacherConflict(constraintFactory),
+                studentGroupConflict(constraintFactory),
+                // Soft constraints are only implemented in the "complete" implementation
+        };
+    }
+
+    private Constraint roomConflict(ConstraintFactory constraintFactory) {
+        // Each room can accommodate at most one lesson at the same time
+        return constraintFactory
+                // Select a pair of 2 different lessons
+                .fromUniquePair(Lesson.class,
+                        // in the same timeslot
+                        Joiners.equal(Lesson::getTimeslot),
+                        // in the same room
+                        Joiners.equal(Lesson::getRoom))
+                // to penalize
+                .penalize("Room conflict", HardSoftScore.ONE_HARD);
+    }
+
+    private Constraint teacherConflict(ConstraintFactory constraintFactory) {
+        // Each teacher can teach at most one lesson at the same time
+        return constraintFactory
+                .fromUniquePair(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.equal(Lesson::getTeacher))
+                .penalize("Teacher conflict", HardSoftScore.ONE_HARD);
+    }
+
+    private Constraint studentGroupConflict(ConstraintFactory constraintFactory) {
+        // Each student can attend at most one lesson at the same time
+        return constraintFactory
+                .fromUniquePair(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.equal(Lesson::getStudentGroup))
+                .penalize("Student group conflict", HardSoftScore.ONE_HARD);
+    }
+
+}
---
+++ initial/src/test/java/com/example/solver/TimeTableServiceTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.solver;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.domain.Lesson;
+import com.example.domain.Room;
+import com.example.domain.TimeTable;
+import com.example.domain.Timeslot;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {
+        "optaplanner.solver.termination.spent-limit=1h", // Effectively disable this termination in favor of the best-score-limit
+        "optaplanner.solver.termination.best-score-limit=0hard/*soft"})
+public class TimeTableServiceTest {
+
+    @Autowired
+    private TimeTableService timeTableService;
+
+    @Test(timeout = 600_000)
+    public void solve() {
+        TimeTable problem = generateProblem();
+        TimeTable solution = timeTableService.solve(problem);
+        assertFalse(solution.getLessonList().isEmpty());
+        for (Lesson lesson : solution.getLessonList()) {
+            assertNotNull(lesson.getTimeslot());
+            assertNotNull(lesson.getRoom());
+        }
+        assertTrue(solution.getScore().isFeasible());
+    }
+
+    private TimeTable generateProblem() {
+        List<Timeslot> timeslotList = new ArrayList<>();
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(8, 30), LocalTime.of(9, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(9, 30), LocalTime.of(10, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(10, 30), LocalTime.of(11, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(13, 30), LocalTime.of(14, 30)));
+        timeslotList.add(new Timeslot(DayOfWeek.MONDAY, LocalTime.of(14, 30), LocalTime.of(15, 30)));
+
+        List<Room> roomList = new ArrayList<>();
+        roomList.add(new Room("Room A"));
+        roomList.add(new Room("Room B"));
+        roomList.add(new Room("Room C"));
+
+        List<Lesson> lessonList = new ArrayList<>();
+        lessonList.add(new Lesson(101L, "Math", "B. May", "9th grade"));
+        lessonList.add(new Lesson(102L, "Physics", "M. Curie", "9th grade"));
+        lessonList.add(new Lesson(103L, "Geography", "M. Polo", "9th grade"));
+        lessonList.add(new Lesson(104L, "English", "I. Jones", "9th grade"));
+        lessonList.add(new Lesson(105L, "Spanish", "P. Cruz", "9th grade"));
+
+        lessonList.add(new Lesson(201L, "Math", "B. May", "10th grade"));
+        lessonList.add(new Lesson(202L, "Chemistry", "M. Curie", "10th grade"));
+        lessonList.add(new Lesson(203L, "History", "I. Jones", "10th grade"));
+        lessonList.add(new Lesson(204L, "English", "P. Cruz", "10th grade"));
+        lessonList.add(new Lesson(205L, "French", "M. Curie", "10th grade"));
+        return new TimeTable(timeslotList, roomList, lessonList);
+    }
+
+}
