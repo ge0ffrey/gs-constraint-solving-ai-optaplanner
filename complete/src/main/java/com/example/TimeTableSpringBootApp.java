00b68c6893ba91bbef0b7f5c5fdc585b3844fd57
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-01-10 11:28:28 +0100

    Assign one lecture already for development

--- complete/src/main/java/com/example/TimeTableSpringBootApp.java
+++ complete/src/main/java/com/example/TimeTableSpringBootApp.java
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class TimeTableSpringBootApp {
@@ -178,8 +179,12 @@ public class TimeTableSpringBootApp {
                 lessonRepository.save(new Lesson("Physical education", "C. Lewis", "12th grade"));
                 lessonRepository.save(new Lesson("Physical education", "C. Lewis", "12th grade"));
                 lessonRepository.save(new Lesson("Physical education", "C. Lewis", "12th grade"));
-
             }
+
+            Lesson lesson = lessonRepository.findAll(Sort.by("id")).iterator().next();
+            lesson.setTimeslot(timeslotRepository.findAll(Sort.by("id")).iterator().next());
+            lesson.setRoom(roomRepository.findAll(Sort.by("id")).iterator().next());
+            lessonRepository.save(lesson);
         };
     }
 
