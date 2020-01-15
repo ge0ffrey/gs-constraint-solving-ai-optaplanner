bae4fe3d313a1e53cefab81cbb178facbdb84c53
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-01-15 14:44:40 +0100

    Add @RequestBody to make REST work

--- initial/src/main/java/com/example/solver/TimeTableController.java
+++ initial/src/main/java/com/example/solver/TimeTableController.java
@@ -8,6 +8,7 @@ import org.optaplanner.core.api.solver.SolverJob;
 import org.optaplanner.core.api.solver.SolverManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class TimeTableController {
     private SolverManager<TimeTable, UUID> solverManager;
 
     @PostMapping("/solve")
-    public TimeTable solve(TimeTable problem) {
+    public TimeTable solve(@RequestBody TimeTable problem) {
         UUID problemId = UUID.randomUUID();
         // Submit the problem to start solving
         SolverJob<TimeTable, UUID> solverJob = solverManager.solve(problemId, problem);
